8f9e9cf446407a445327d6d84826d2a2e90e9665
Ewa Czekalska <ewac@microsoft.com>
2016-05-19 18:12:26 +0100

    Initial Commit

---
+++ Coursera-project2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Coursera-project2
+//
+//  Created by ewac on 19/05/2016.
+//  Copyright © 2016 Ewa Czekalska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Coursera-project2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Coursera-project2
+//
+//  Created by ewac on 19/05/2016.
+//  Copyright © 2016 Ewa Czekalska. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
